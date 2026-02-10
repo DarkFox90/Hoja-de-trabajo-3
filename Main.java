ab22271bfa34da3c792cbedc320f000992318123
DarkFox90 <carlospozuelos3@gmail.com>
2026-02-09 19:46:28 -0600

    generado archivo con numeros random

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        FileAssistant.GenerateNumbers("Numeros.txt", 3000);
     }
 }
