5f3608b317cf8d59bf26190664977ebb87a29e9a
DarkFox90 <carlospozuelos3@gmail.com>
2026-02-09 19:43:51 -0600

    añadido metodo para generar numeros random y gitignore

--- FileAssistant.java
+++ FileAssistant.java
@@ -1,3 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+
 public class FileAssistant {
-    
+    public static void GenerateNumbers(String name, int quantity) throws IOException  {
+        Random random = new Random();
+        
+        BufferedWriter writer = new BufferedWriter(new FileWriter(name));
+
+        for (int i = 0; i < quantity; i++) {
+            int number = random.nextInt(10000);
+            writer.write(Integer.toString(number));
+            writer.newLine();
+        }
+
+        writer.close();
+    }    
 }
