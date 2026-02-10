2a7608fb2f09c9ad86e26c773c5c3c7e2e8ba563
DarkFox90 <carlospozuelos3@gmail.com>
2026-02-09 18:47:13 -0600

    finalizados los sorts

--- GnomeSort.java
+++ GnomeSort.java
@@ -1,3 +1,22 @@
 public class GnomeSort {
-    
+
+    public static <T extends Comparable<T>> void sort(T[] arr) {
+        int index = 0;
+
+        while (index < arr.length) {
+            if (index == 0) {
+                index++;
+            }
+            else if (arr[index].compareTo(arr[index - 1]) >= 0) {
+                index++;
+            }
+            else {
+                T temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+    }
 }
+
--- RadixSort.java
+++ RadixSort.java
@@ -1,3 +1,40 @@
+import java.util.Arrays;
 public class RadixSort {
-    
+    public static void sort(Integer[] arr) {
+        if (arr.length == 0) return;
+        int m = getMax(arr, arr.length);
+        
+        
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, arr.length, exp);
+    }
+
+    private static int getMax(Integer[] arr, int n) {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+
+    private static void countSort(Integer[] arr, int n, int exp) {
+        Integer[] output = new Integer[n]; 
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
 }
+
