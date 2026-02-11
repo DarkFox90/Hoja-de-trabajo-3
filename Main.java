a03951a06b8bc9b34a92bb37a857d22c578c9694
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-02-10 18:47:59 -0600

    Creacion de los sort ordenados

--- Main.java
+++ Main.java
@@ -11,18 +11,39 @@ public class Main {
         System.in.read();
 
         for (int tamano : tamanio) {
+
             Integer[] original = FileAssistant.ReadNumbers("Numeros.txt");
+
+            Integer[] ordered = Arrays.copyOf(original, original.length);
+            MergeSort.sort(ordered);
+
             QuickSort(original);
+            QuickSortOrdenado(ordered);
+
             MergeSort(original);
+            MergeSortOrdenado(ordered);
+
             GnomeSort(original);
+            GnomeSortOrdenado(ordered);
+
             InsertionSort(original);
+            InsertionSortOrdenado(ordered);
+
             RadixSort(original);
+            RadixSortOrdenado(ordered);
         }
     }
 
     private static void QuickSort(Integer[] original) {
         for (int i = 0; i < Iteraciones; i++) {
             Integer[] copia = Arrays.copyOf(original, original.length);
+            QuickSort.sort(copia);
+        }
+    }
+
+    private static void QuickSortOrdenado(Integer[] ordered) {
+        for (int i = 0; i < Iteraciones; i++) {
+            Integer[] copia = Arrays.copyOf(ordered, ordered.length);
             QuickSort.sort(copia);
         }
     }
@@ -34,9 +55,23 @@ public class Main {
         }
     }
 
+    private static void MergeSortOrdenado(Integer[] ordered) {
+        for (int i = 0; i < Iteraciones; i++) {
+            Integer[] copia = Arrays.copyOf(ordered, ordered.length);
+            MergeSort.sort(copia);
+        }
+    }
+
     private static void GnomeSort(Integer[] original) {
         for (int i = 0; i < Iteraciones; i++) {
             Integer[] copia = Arrays.copyOf(original, original.length);
+            GnomeSort.sort(copia);
+        }
+    }
+
+    private static void GnomeSortOrdenado(Integer[] ordered) {
+        for (int i = 0; i < Iteraciones; i++) {
+            Integer[] copia = Arrays.copyOf(ordered, ordered.length);
             GnomeSort.sort(copia);
         }
     }
@@ -48,9 +83,23 @@ public class Main {
         }
     }
 
+    private static void InsertionSortOrdenado(Integer[] ordered) {
+        for (int i = 0; i < Iteraciones; i++) {
+            Integer[] copia = Arrays.copyOf(ordered, ordered.length);
+            InsertionSort.sort(copia);
+        }
+    }
+
     private static void RadixSort(Integer[] original) {
         for (int i = 0; i < Iteraciones; i++) {
             Integer[] copia = Arrays.copyOf(original, original.length);
+            RadixSort.sort(copia);
+        }
+    }
+
+    private static void RadixSortOrdenado(Integer[] ordered) {
+        for (int i = 0; i < Iteraciones; i++) {
+            Integer[] copia = Arrays.copyOf(ordered, ordered.length);
             RadixSort.sort(copia);
         }
     }
